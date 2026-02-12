cb062950c206f5f9a24b5a53ca9923d757e0f830
kochanyeol <cksdufqqpu@gmail.com>
2026-02-12 15:00:52 +0900

    Initial Commit

--- OZ_CODING_FASTAPI_과제/DAY_1_과제/test_simple.py
+++ OZ_CODING_FASTAPI_과제/DAY_1_과제/test_simple.py
@@ -1,14 +1,43 @@
 from datetime import datetime, timedelta
 
-DELIVERY_DATE = 2
 
-def get_eta(purchase_date: datetime) -> datetime:
+DELIVERY_DAYS = 2
+
+
+def _is_holiday(day: datetime) -> bool:
     return day.weekday() >= 5
 
-def test_eta() -> None:
 
-    purchase_date = datetime(2020, 1, 2)
+def get_eta(purchase_date: datetime) -> datetime:
+    current_date = purchase_date
+    remaining_days = DELIVERY_DAYS
 
-    result = get_eta(purchase_date)
+    while remaining_days > 0:
+        current_date += timedelta(days=1)
+        if not _is_holiday(current_date):
+            remaining_days -= 1
 
-    assert result == datetime(2020, 1, 2)
+    return current_date
+
+
+def test_get_eta_2023_12_01() -> None:
+    result = get_eta(datetime(2023, 12, 1))
+    assert result == datetime(2023, 12, 4)
+
+
+def test_get_eta_2024_12_31() -> None:
+    """
+    공휴일 정보가 없어서 1월 1일도 평일로 취급됩니다.
+    """
+    result = get_eta(datetime(2024, 12, 31))
+    assert result == datetime(2025, 1, 2)
+
+
+def test_get_eta_2024_02_28() -> None:
+    result = get_eta(datetime(2024, 2, 28))
+    assert result == datetime(2024, 3, 1)
+
+
+def test_get_eta_2023_02_28() -> None:
+    result = get_eta(datetime(2023, 2, 28))
+    assert result == datetime(2023, 3, 2)
