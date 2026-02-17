3d6dcae6e5857f9291d3fb6f737f7c077233db39
kochanyeol <cksdufqqpu@gmail.com>
2026-02-17 12:19:01 +0900

    update

---
+++ OZ_CODING_FASTAPI_과제/DAY_2_과제/temp.py
@@ -0,0 +1,10 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+def mul(a: int, b: int) -> int:
+    return a * b
+
+# def test_mul() -> None:
+#     a, b = 1, 2
+#     result = mul(a, b)
+#     assert result == 2
