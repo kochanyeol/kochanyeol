8414e9c241014a5cbafd8e8e0f56bd49f2e26b2c
kochanyeol <cksdufqqpu@gmail.com>
2026-03-19 17:15:09 +0900

    DJANGO DAY3 TEST

--- OZ_CODING_DJANGO_과제/DAY_3_과제/config/urls.py
+++ OZ_CODING_DJANGO_과제/DAY_3_과제/config/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', include('todo.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('user.urls')),
+    path('users/', include('user.urls')),
 ]
