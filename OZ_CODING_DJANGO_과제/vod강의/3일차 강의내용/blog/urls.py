31b3bb42836b9323927a2928e166339d4d482fa2
kochanyeol <cksdufqqpu@gmail.com>
2026-03-19 17:19:23 +0900

    DJANGO DAY4 TEST

--- OZ_CODING_DJANGO_과제/vod강의/3일차 강의내용/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from blog import views
-
-urlpatterns = [
-    path('', views.blog_list, name='blog_list'),
-    path('<int:pk>/', views.blog_detail, name='blog_detail'),
-]
