d8041ac611b2323c5b3eb00cd357a74d046442b5
kochanyeol <cksdufqqpu@gmail.com>
2026-03-20 18:04:07 +0900

    DJANGO DAY4 TEST

--- OZ_CODING_DJANGO_과제/DAY_4_과제/todo/urls.py
+++ OZ_CODING_DJANGO_과제/DAY_4_과제/todo/urls.py
@@ -3,8 +3,8 @@ from .views import todo_list, todo_info, todo_create, todo_delete, todo_update
 
 urlpatterns = [
     path('', todo_list, name='todo_list'),
-    path('<int:pk>/', todo_info, name='todo_info'),
+    path('<int:id>/', todo_info, name='todo_info'),
     path('create/', todo_create, name='todo_create'),
-    path('<int:pk>/update/', todo_update, name='todo_update'),
-    path('<int:pk>/delete/', todo_delete, name='todo_delete'),
+    path('<int:id>/update/', todo_update, name='todo_update'),
+    path('<int:id>/delete/', todo_delete, name='todo_delete'),
 ]
--- OZ_CODING_DJANGO_과제/DAY_4_과제/todo/views.py
+++ OZ_CODING_DJANGO_과제/DAY_4_과제/todo/views.py
@@ -18,9 +18,9 @@ def todo_list(request):
     return render(request, 'todo/todo_list.html', context)
 
 @login_required  # 로그인 안 된 유저는 로그인 페이지로 자동 redirect(django 기능)
-def todo_info(request, pk):
-    # url에서 넘어온 todo_id로 Todo 객체를 가져오고 없으면 404 반환
-    todo = get_object_or_404(Todo, pk=pk, user=request.user)
+def todo_info(request, id):
+    # url에서 넘어온 id로 Todo 객체를 가져오고 없으면 404 반환
+    todo = get_object_or_404(Todo, pk=id, user=request.user)
     context = {
         # todo 객체의 필드들을 딕셔너리 형태로 변환해서 전달
         'todo': todo.__dict__
@@ -37,30 +37,30 @@ def todo_create(request):
             todo = form.save(commit=False)  # DB 저장 전 Todo 객체만 생성 (아직 user 없음)
             todo.user = request.user  # 로그인한 유저를 Todo 객체에 추가
             todo.save()  # user까지 채운 Todo 객체를 DB에 최종 저장
-            return redirect('todo_info', pk=todo.pk)  # 생성된 Todo 상세 페이지로 이동
+            return redirect('todo_info', id=todo.pk)  # 생성된 Todo 상세 페이지로 이동
     else:
         # GET 요청 시 빈 폼을 렌더링
         form = TodoForm()
     return render(request, 'todo/todo_create.html', {'form': form})
 
 @login_required  # 로그인 안 된 유저는 로그인 페이지로 자동 redirect
-def todo_update(request, pk):
-    # pk와 로그인한 유저가 일치하는 Todo 가져오기 (없으면 404 반환)
-    todo = get_object_or_404(Todo, pk=pk, user=request.user)
+def todo_update(request, id):
+    # id와 로그인한 유저가 일치하는 Todo 가져오기 (없으면 404 반환)
+    todo = get_object_or_404(Todo, pk=id, user=request.user)
     if request.method == 'POST':
         # 수정된 데이터를 기존 Todo 객체(instance)에 덮어씌우기
         form = TodoUpdateForm(request.POST, instance=todo)
         if form.is_valid():  # 입력값이 유효한지 검사
             form.save()  # 수정된 Todo를 DB에 저장 (user는 이미 있으므로 commit=False 불필요)
-            return redirect('todo_info', pk=todo.pk)  # 수정된 Todo 상세 페이지로 이동
+            return redirect('todo_info', id=todo.pk)  # 수정된 Todo 상세 페이지로 이동
     else:
         # GET 요청 시 기존 데이터가 채워진 폼을 렌더링
         form = TodoUpdateForm(instance=todo)
     return render(request, 'todo/todo_update.html', {'form': form})
 
 @login_required  # 로그인 안 된 유저는 로그인 페이지로 자동 redirect
-def todo_delete(request, pk):
-    # pk와 로그인한 유저가 일치하는 Todo 가져오기 (없으면 404 반환)
-    todo = get_object_or_404(Todo, pk=pk, user=request.user)
+def todo_delete(request, id):
+    # id와 로그인한 유저가 일치하는 Todo 가져오기 (없으면 404 반환)
+    todo = get_object_or_404(Todo, pk=id, user=request.user)
     todo.delete()  # Todo를 DB에서 삭제
     return redirect('todo_list')  # 삭제 후 목록 페이지로 이동
